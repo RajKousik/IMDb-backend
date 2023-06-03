5485928c5ddb7a87446b984c6a87ac8d7442d442
Raj Kousik <rajkousik20@gmail.com>
2023-06-03 13:30:35 +0530

    Added DB Schema

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const app = express()
 
 const PORT = 4500
 
+const mongoose = require('mongoose') 
+
+const movieRating = require('./routes/movieRating.js')
+
+
 console.log('From app.js');
 
 app.get('/', (req, res)=>{
     res.send('Welcome')
 })
+
+app.use('/api/v1/movieRatings', movieRating);
 
 app.listen(PORT, ()=>{
     console.log(`API is working on ${PORT}`);
---
+++ models/movieRating.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const movieRatingSchema = new mongoose.Schema({
+    name : {
+        type : String, 
+        required : true
+    },
+    genre : {
+        type : String, 
+        required : true
+    },
+    language : {
+        type : String, 
+        default : "Unknown"
+    },
+    releasedYear : {
+        type : String, 
+        required : true
+    },
+    rating : {
+        type : String, 
+        required : true
+    }
+    
+})
+
+module.exports = mongoose.model('movieRatingModel', movieRatingSchema);
