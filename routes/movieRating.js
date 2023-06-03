f440837f2752b938873d8be4b421c45897585054
Raj Kousik <rajkousik20@gmail.com>
2023-06-03 14:57:32 +0530

    Added Controllers

--- routes/movieRating.js
+++ routes/movieRating.js
@@ -1,130 +1,145 @@
 const express = require('express')
 const router = express.Router();
 
-const movieRatingsModel = require('../models/movieRating');
-
+//checking
 // router.get('/', (req, res)=>{
 //     res.send("Hello");
 // })
 
-router.get('/', async(req,res)=>{
-    try
-    {
-        const movieRatings = await movieRatingsModel.find();
-        res.status(200).json(movieRatings);
-    }
-    catch(err)
-    {
-        // console.log(err);
-        res.status(500).json({message : err.message});
-    }
-})
 
-router.post('/', async (req, res)=>{
+const movieRatingsModel = require('../models/movieRating');
 
-    const newMovie = new movieRatingsModel({
-        name : req.body.name,
-        genre : req.body.genre,
-        language : req.body.language,
-        releasedYear : req.body.releasedYear,
-        rating : req.body.rating
-    })
 
-    try
-    {
-        const movie = await newMovie.save();
-        res.status(201).json(movie);
-    }
-    catch(err)
-    {
-        // console.log(err);
-        res.status(500).json({message : err.message});
-    }
+const {getAllMovieRatings, createNewMovie, getMovie, getMovieRating, updateMovie, deleteMovie} = require('../controllers/movieRating.js')
 
-})
 
-router.get('/:id', getMovie,(req, res)=>{
+router.route('/').get(getAllMovieRatings);
+router.route('/').post(createNewMovie);
 
-    try{   
-        res.status(200).json(res.movie);
-    }
-    catch(err)
-    {
-        // console.log(err);
-        res.status(500).json({message : err.message});
-    }
-
-})
-
-router.patch('/:id', getMovie, async(req, res)=>{
-
-    if(req.body.name != null)
-    {
-        res.movie.name = req.body.name;
-    }
-    if(req.body.genre != null)
-    {
-        res.movie.genre = req.body.genre;
-    }
-    if(req.body.language != null)
-    {
-        res.movie.language = req.body.language;
-    }
-    if(req.body.releasedYear != null)
-    {
-        res.movie.releasedYear = req.body.releasedYear;
-    }
-    if(req.body.rating != null)
-    {
-        res.movie.rating = req.body.rating;
-    }
-
-    try
-    {
-        const updatedMovie = await res.movie.save();
-        res.status(200).json(updatedMovie);
-    }
-    catch(err)
-    {
-        // console.log(err);
-        res.status(400).json({message : err.message});
-    }
-
-})
-
-router.delete('/:id', getMovie, async (req, res)=>{
-    try{
-        await res.movie.deleteOne();
-        res.status(200).json({message : `Deleted Movie : ${res.movie.name}`});
-    }
-    catch(err)
-    {
-        res.status(400).json({message: error.message})
-    }
-})
+router.route('/:id').get(getMovie, getMovieRating);
+router.route('/:id').patch(getMovie, updateMovie);
+router.route('/:id').delete(getMovie, deleteMovie);
 
 
 
-async function getMovie(req, res, next){
+// router.get('/', async(req,res)=>{
+//     try
+//     {
+//         const movieRatings = await movieRatingsModel.find();
+//         res.status(200).json(movieRatings);
+//     }
+//     catch(err)
+//     {
+//         // console.log(err);
+//         res.status(500).json({message : err.message});
+//     }
+// })
 
-    let movie;
+// router.post('/', async (req, res)=>{
 
-    try{
+//     const newMovie = new movieRatingsModel({
+//         name : req.body.name,
+//         genre : req.body.genre,
+//         language : req.body.language,
+//         releasedYear : req.body.releasedYear,
+//         rating : req.body.rating
+//     })
 
-        movie = await movieRatingsModel.findById(req.params.id);
-        if(movie == null)
-        {
-            return res.status(404).json({Message : "Not Found"});
-        }
+//     try
+//     {
+//         const movie = await newMovie.save();
+//         res.status(201).json(movie);
+//     }
+//     catch(err)
+//     {
+//         // console.log(err);
+//         res.status(500).json({message : err.message});
+//     }
 
-    }
-    catch(err)
-    {
-        // console.log(err);
-        res.status(500).json({message : err.message});
-    }
-    res.movie = movie;
-    next()
-}
+// })
+
+// router.get('/:id', getMovie,(req, res)=>{
+
+//     try{   
+//         res.status(200).json(res.movie);
+//     }
+//     catch(err)
+//     {
+//         // console.log(err);
+//         res.status(500).json({message : err.message});
+//     }
+
+// })
+
+// router.patch('/:id', getMovie, async(req, res)=>{
+
+//     if(req.body.name != null)
+//     {
+//         res.movie.name = req.body.name;
+//     }
+//     if(req.body.genre != null)
+//     {
+//         res.movie.genre = req.body.genre;
+//     }
+//     if(req.body.language != null)
+//     {
+//         res.movie.language = req.body.language;
+//     }
+//     if(req.body.releasedYear != null)
+//     {
+//         res.movie.releasedYear = req.body.releasedYear;
+//     }
+//     if(req.body.rating != null)
+//     {
+//         res.movie.rating = req.body.rating;
+//     }
+
+//     try
+//     {
+//         const updatedMovie = await res.movie.save();
+//         res.status(200).json(updatedMovie);
+//     }
+//     catch(err)
+//     {
+//         // console.log(err);
+//         res.status(400).json({message : err.message});
+//     }
+
+// })
+
+// router.delete('/:id', getMovie, async (req, res)=>{
+//     try{
+//         await res.movie.deleteOne();
+//         res.status(200).json({message : `Deleted Movie : ${res.movie.name}`});
+//     }
+//     catch(err)
+//     {
+//         res.status(400).json({message: error.message})
+//     }
+// })
+
+
+
+// async function getMovie(req, res, next){
+
+//     let movie;
+
+//     try{
+
+//         movie = await movieRatingsModel.findById(req.params.id);
+//         if(movie == null)
+//         {
+//             return res.status(404).json({Message : "Not Found"});
+//         }
+
+//     }
+//     catch(err)
+//     {
+//         // console.log(err);
+//         res.status(500).json({message : err.message});
+//     }
+//     res.movie = movie;
+//     next()
+// }
 
 module.exports = router;
